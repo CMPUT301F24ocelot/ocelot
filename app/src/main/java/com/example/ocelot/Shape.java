8a3fd4c27a381799e9ad83650bf07630c5ba0ac6
Rishav Kumar <rishavkr1766@gmail.com>
2024-09-27 00:17:15 -0600

    Add new color string field

--- app/src/main/java/com/example/ocelot/Shape.java
+++ app/src/main/java/com/example/ocelot/Shape.java
@@ -3,7 +3,8 @@ package com.example.ocelot;
 public abstract class Shape {
     protected int x;
     protected int y;
-
+    protected String color = "blue";
+    
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
