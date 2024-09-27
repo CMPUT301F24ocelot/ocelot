3dc546e3187eb1f845bbcc6fe77dd8a8dd0bdd5b
Dipen Kumar <dipenbachani@gmail.com>
2024-09-27 14:29:53 -0600

    Added color field yellow to Shape class

--- app/src/main/java/com/example/ocelot/Shape.java
+++ app/src/main/java/com/example/ocelot/Shape.java
@@ -3,7 +3,7 @@ package com.example.ocelot;
 public abstract class Shape {
     protected int x;
     protected int y;
-    protected String color = "blue";
+    protected String color = "yellow";
 
     public Shape(int x, int y) {
         this.x = x;
